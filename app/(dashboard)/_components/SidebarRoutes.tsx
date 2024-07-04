dd2dc97fa277db17adb78807d05804a5a51cb99d
S YASHWANTH <yashwantsalukuti@gmail.com>
2024-07-04 17:22:14 +0530

    Layout

---
+++ app/(dashboard)/_components/SidebarRoutes.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Layout } from "lucide-react";
+import { Compass } from "lucide-react";
+import { SidebarItem } from "./SidebarItem";
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Browse",
+    href: "/search",
+  },
+];
+
+export const SidebarRoutes = () => {
+  const routes = guestRoutes;
+
+  return (
+    <div className="flex flex-col w-full ">
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
