f884deca88cb2b616cb0ab29838ec912397071dd
yashwanthsalukuti <160753806+yashwanthsalukuti@users.noreply.github.com>
2024-08-01 20:02:24 +0530

    Update TOKEN

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -5,10 +5,10 @@ import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { isTeacher } from "@/lib/teacher";
 
-const { video } = new Mux(
-  process.env.MUX_TOKEN_ID,
-  process.env.MUX_TOKEN_SECRET
-);
+const { video } = new Mux({
+  tokenId:process.env.MUX_TOKEN_ID,
+  tokenSecret: process.env.MUX_TOKEN_SECRET
+});
 
 export async function DELETE(
   req: Request,
